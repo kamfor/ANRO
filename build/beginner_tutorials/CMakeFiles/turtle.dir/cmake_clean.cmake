fb611095159bb76373d857accb6aec74a5c55004
kamfor <kfforex@gmail.com>
2017-03-27 10:39:58 +0200

    initial

---
+++ build/beginner_tutorials/CMakeFiles/turtle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtle.dir/src/turtle.cpp.o"
+  "/home/kforysze/catkin_ws/devel/lib/beginner_tutorials/turtle.pdb"
+  "/home/kforysze/catkin_ws/devel/lib/beginner_tutorials/turtle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turtle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
