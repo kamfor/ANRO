dd868afbfdaf7232f79a3c176dcb583c0f9c6340
kamfor <kfforex@gmail.com>
2017-04-05 11:35:50 -0700

    sds

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kforysze/ANRO/src
-# Build directory: /home/kforysze/ANRO/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(beginner_tutorials)
--- build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kforysze/ANRO/src/beginner_tutorials
-# Build directory: /home/kforysze/ANRO/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
