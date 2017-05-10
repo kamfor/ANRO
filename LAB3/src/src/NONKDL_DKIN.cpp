c32c64cfea4aea88b590a9f184f03845d3e81692
kamfor <kfforex@gmail.com>
2017-05-10 13:03:29 -0700

    NONKDL

--- LAB3/src/src/NONKDL_DKIN.cpp
+++ LAB3/src/src/NONKDL_DKIN.cpp
@@ -4,24 +4,36 @@
 #include <visualization_msgs/Marker.h>
 
 float finalX, finalY, finalZ;
+ros::Publisher pub; 
 
 void jointCallback(const sensor_msgs::JointState & msg)
 {
-	ROS_INFO("pose: [%f]", msg.position[1]);
+	ROS_INFO("poseX: [%f]", msg.position[0]);
+	ROS_INFO("poseY: [%f]", msg.position[1]);
+	ROS_INFO("poseZ: [%f]", msg.position[2]);
+	ros::NodeHandle n;
 	
+	geometry_msgs::PoseStamped poseStamped;
 
 	
-	/*finalX = 2 - msg.position[0];
-	finalY = 2 - msg.position[1];
-	finalZ = 2 - msg.position[2];
+		
+	poseStamped.header.frame_id="base_link";
+	poseStamped.header.stamp = ros::Time::now();
 	
-	poseStamped.pose.position.x=finalX;
-	poseStamped.pose.position.y=finalY;
-	poseStamped.pose.position.z=finalZ;
 
-	poseStamped.pose.orientation.x=1;
-	poseStamped.pose.orientation.y=1;
-	poseStamped.pose.orientation.z=1;*/
+	finalX = msg.position[0] ;
+	finalY = msg.position[1] ;
+	finalZ = msg.position[2] ;
+	
+	poseStamped.pose.position.x=finalZ-2;
+	poseStamped.pose.position.y=finalY-2;
+	poseStamped.pose.position.z=2-finalX;
+
+	poseStamped.pose.orientation.x=0;
+	poseStamped.pose.orientation.y=0;
+	poseStamped.pose.orientation.z=0;
+	poseStamped.pose.orientation.w=0; 
+	pub.publish(poseStamped);
 }
 
 
@@ -29,35 +41,10 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "NONKDL_DKIN");
 	ros::NodeHandle s; 
-
+	ros::Rate loop_rate(30);
+	pub = s.advertise<geometry_msgs::PoseStamped>("nonkdl_pose",1);
 	ros::Subscriber sub=s.subscribe("joint_states", 100, jointCallback);
 	
-	ros::Rate loop_rate(30);
-	ros::NodeHandle n;
-	ros::Publisher pub=n.advertise<geometry_msgs::PoseStamped>("base_link",1);
-	geometry_msgs::PoseStamped poseStamped;
-
-	
-		while(ros::ok()){
-		poseStamped.header.frame_id="/frame";
-		poseStamped.header.stamp = ros::Time::now();
-	
-		poseStamped.pose.position.x=2;
-		poseStamped.pose.position.y=3;
-		poseStamped.pose.position.z=4;
-	
-		poseStamped.pose.orientation.x=1;
-		poseStamped.pose.orientation.y=1;
-		poseStamped.pose.orientation.z=1;
-		poseStamped.pose.orientation.w=1;
-
-	
-		pub.publish(poseStamped);
-
-		loop_rate.sleep();
-		}
-	
-
 	ros::spin();
 
 	return 0;
